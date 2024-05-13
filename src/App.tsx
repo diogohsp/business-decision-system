4ed6240f69009d1e7ad0a3bb76836d50bced6aaf
Texeraaa <pedro.henrique.ph2018@hotmail.com>
2024-05-12 22:06:36 -0300

    configure themeProvider and making header

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { Helmet, HelmetProvider } from "react-helmet-async";
 import { Toaster } from "sonner";
+import { ThemeProvider } from "./components/theme/theme-provider";
 
 function App() {
   return (
     <>
+    <ThemeProvider storageKey="business-decision-theme" defaultTheme="dark">
       <HelmetProvider>
         <Helmet titleTemplate="%s | decisionSystem" />
         <RouterProvider router={router}></RouterProvider>
         <Toaster richColors />
       </HelmetProvider>
+    </ThemeProvider>
     </>
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,26 +1,30 @@
+import { Building2, FileText, Home } from 'lucide-react'
+import { Separator } from './ui/separator'
+import { NavLink } from './nav-link'
+import { ThemeToggle } from './theme/theme-toggle'
+
 export function Header() {
   return (
-    <>
-      <nav>
-        <ul className="list-none w-full flex justify-center gap-x-4 p-6 bg-sky-100">
-          <li>
-            <a
-              href="#"
-              className="hover:bg-slate-300 hover:p-2 hover:rounded-sm"
-            >
-              Home
-            </a>
-          </li>
-          <li>
-            <a
-              href="#listagem"
-              className="hover:bg-slate-300 hover:p-2 hover:rounded-sm"
-            >
-              Listagem
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </>
-  );
+    <div className="border-b">
+      <div className="flex h-16 items-center gap-6 px-6">
+        <Building2 className="h-6 w-6" />
+
+        <Separator orientation="vertical" className="h-6" />
+
+        <nav className="flex items-center space-x-4 lg:space-x-6">
+          <NavLink to={'/'}>
+            <Home className="h-4 w-4" />
+            Inicio
+          </NavLink>{' '}
+          <NavLink to={'/orders'}>
+            <FileText className="h-4 w-4" />
+            Listagem
+          </NavLink>
+        </nav>
+        <div className="ml-auto flex items-center gap-2">
+          <ThemeToggle />
+        </div>
+      </div>
+    </div>
+  )
 }
